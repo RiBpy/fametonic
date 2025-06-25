ad1f11698367c654ed28f672c82e87fa1ad69295
ribpy <mdriazbappy22@gmail.com>
2025-06-26 02:03:10 +0600

    TopBanner mobile view fixed

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const TopBanner = () => (
-  <div className="bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-400 py-3 text-center overflow-hidden global-padding font-figtree">
-    <div className="z-10 flex items-start justify-center gap-2 font-bold text-white text-[22px">
+  <div className="bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-400 py-3 overflow-hidden global-padding font-figtree text-center">
+    <div className="z-10 w-full space-x-1 font-bold text-white text-[22px">
       <span className="text-lg">🚀</span>
-      <span className="drop-shadow-lg text-[16px] md:text-[22px] font-extrabold">
+      <span className="text-[16px] md:text-[22px] font-extrabold">
         <span className="text-cyan-200 ">FRESH BEGINNINGS SALE:</span> Extra 25%
         OFF, Limited Spots - start your journey today!
       </span>
