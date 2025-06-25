3acf3236b2a72c482a76d616054d46565c9cc1c9
ribpy <mdriazbappy22@gmail.com>
2025-06-26 02:33:23 +0600

    updated gradient background of topBanner

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const TopBanner = () => (
-  <div className="bg-gradient-to-r from-red-500 via-purple-400 to-cyan-400 py-3 overflow-hidden global-padding font-figtree text-center">
+<div className="bg-gradient-to-r from-[#E91E63] from-0% via-[#8B5CF6] via-75% to-[#06B6D4] to-100% py-3 overflow-hidden global-padding font-figtree text-center">
     <div className="z-10 w-full space-x-1 font-bold text-white text-[22px">
       <span className="text-lg">🚀</span>
       <span className="text-[16px] md:text-[22px] font-extrabold">
-        <span className="text-cyan-200 ">FRESH BEGINNINGS SALE:</span> Extra 25%
+        <span className="text-[#00E7F9] ">FRESH BEGINNINGS SALE:</span> Extra 25%
         OFF, Limited Spots - start your journey today!
       </span>
     </div>
